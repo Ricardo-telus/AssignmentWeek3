515bbb977350446e13473b3f27c85ac02f98d86c
Ricardo-telus <ricardo.perez@telus.com>
2022-03-14 15:50:59 -0600

    assignment finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import EntryComponent from './EntryComponent/EntryComponent';
+import EntryFormComponent from './EntryFormComponent/EntryFormComponent';
+import { useState, useEffect } from 'react'
+import { client } from './apiConten'
+const array = [
+  { tittle: "Blog 1", content:"Contenido 1", author:"Javier Cabrera"},  
+]
 function App() {
+  const [entries, setEntries] = useState(array)
+  const [content, setContent]=useState(false)
+  useEffect(() => {      
+    client.getEntries()
+    .then((response)=>{
+        response.items.forEach(element => {
+          setEntries(entries => entries.concat({tittle:element.fields.tittle, content:element.fields.description, author:element.fields.author}))        
+        });
+        setContent(true)
+      })
+    .catch(console.error)          
+},[]);
+  const addNewEntry = (entry) => {
+    const newEntries = [...entries, entry]
+    setEntries(newEntries)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <EntryFormComponent postNewEntry={addNewEntry} />
+      {
+        content?(
+          entries.map(element => (
+            <EntryComponent title={element.tittle} content={element.content} author={element.author} key={element.tittle}/>                    
+          ))
+        ):(<p>cargando</p>)        
+      }
     </div>
-  );
+  )
 }
 
 export default App;
